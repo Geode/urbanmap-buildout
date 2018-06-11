d852668390eea0f9616a99da661fae9716dc1b8e
ndufrane <dufrane.nicolas@gmail.com>
2018-06-11 14:30:05 +0200

    Allow bigger headers

--- uwsgi.sh
+++ uwsgi.sh
@@ -5,6 +5,7 @@ echo Starting uWSGI.
 exec /usr/local/bin/uwsgi \
     --paste config:/srv/urbanmap_buildout/geode.urbanmap/configuration.ini \
     --socket :8000 \
+    --buffer-size=32768 \
     -M \
     -p 4 \
     -H /srv/urbanmap_buildout \
